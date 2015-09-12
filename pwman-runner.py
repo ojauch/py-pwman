871dfc7fd1856e68f4607cc5d84e225668103838
Oskar Jauch <oskar.jauch@gmail.com>
2015-09-12 12:24:42 +0200

    Create setup source tree

---
+++ pwman-runner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""Convenience wrapper for running pwman directly from source tree."""
+
+from pwman.pwman import main
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""setup.py: setuptools control."""
+
+import re
+from setuptools import setup
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('pwman/pwman.py').read(),
+    re.M
+).group(1)
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("uft-8")
+
+setup(
+    name = "py-pwman",
+    packages = ["pwman"],
+    entry_points = {
+        "console_scripts": ['pwman = pwman.pwman:main']
+        },
+    version = version,
+    description = "Python command line password manager.",
+    long_description = long_descr,
+    author = "Oskar Jauch",
+    author_email = "oskar.jauch@gmail.com",
+    url = "https://github.com/ossi96/py-pwman"
+)
